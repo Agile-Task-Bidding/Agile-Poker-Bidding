762bd6b4ea2661233b3dcbd6f75574ca6a7c1bd1
Keenan Turley <keenan.turley@gmail.com>
2020-10-26 05:08:09 -0400

    Add sample config and env files

---
+++ ui/src/config.example.js
@@ -0,0 +1,9 @@
+/**
+ * This is the sample configuration file.
+ * If no config.js is present in the src folder, copy this one
+ * and change the config to your liking before building.
+ */
+export default {
+    WEBSOCKET_URL: 'http://localhost:8000',
+    WEBSITE_URL: 'http://localhost:80'
+}
