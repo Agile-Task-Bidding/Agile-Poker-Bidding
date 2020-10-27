f86016d15b264d849f94d98394c933a8c9388bc3
magdalenapasternak <maggpass@gmail.com>
2020-10-27 18:20:46 -0400

    added comment

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './root/root';
-import store, { history } from './data/state/store';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Root from './root/root'
+import store, { history } from './data/state/store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Root store={store} history={history} />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
+// hey
