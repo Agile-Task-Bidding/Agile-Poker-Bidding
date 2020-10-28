bbcc8fd22a94d69101da464ed282076a4949c9f0
magdalenapasternak <maggpass@gmail.com>
2020-10-27 20:57:09 -0400

    edited css> box shadow, got rid of login on top

--- ui/src/components/login/register.jsx
+++ ui/src/components/login/register.jsx
@@ -11,7 +11,7 @@ export class Register extends React.Component{
 
     render(){
         return <div className="base-container" ref={this.props.containerRef}>
-            <div className="header">REGISTER</div>
+            {/* <div className="header">REGISTER</div> */}
             <div className="content">
                 <div className="image">
                     <img src={loginImg}/>
