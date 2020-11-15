a27cff3d7a03582629a4ec8b0c2299dc77d37d47
magdalenapasternak <maggpass@gmail.com>
2020-11-15 02:25:12 -0500

    changes to the display name page

--- ui/src/pages/round/DisplayNameSubpage.js
+++ ui/src/pages/round/DisplayNameSubpage.js
@@ -1,66 +1,104 @@
 import React, { useState, useEffect } from 'react'
+import './Styling.css'
+import mainImg from '../../components/icon/logo.svg'
 import { connect } from 'react-redux'
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom'
 import ResponsiveContainer from '../../components/ResponsiveContainer'
 import { TextField, Button, CircularProgress } from '@material-ui/core'
 import {
-    createRoomServiceConnection,
-    emitEvent,
-} from '../../data/state/room-service/room-service.actions';
-import { setDisplayName } from '../../data/state/app-data/app-data.actions';
-import { roomServiceSocketSelector } from '../../data/state/room-service/room-service.selectors';
-import { displayNameSelector } from '../../data/state/app-data/app-data.selector';
-import { setAppState } from '../../data/state/app-data/app-data.actions';
+  createRoomServiceConnection,
+  emitEvent,
+} from '../../data/state/room-service/room-service.actions'
+import { setDisplayName } from '../../data/state/app-data/app-data.actions'
+import { roomServiceSocketSelector } from '../../data/state/room-service/room-service.selectors'
+import { displayNameSelector } from '../../data/state/app-data/app-data.selector'
+import { setAppState } from '../../data/state/app-data/app-data.actions'
 
-const DisplayNameSubpage = ({ displayName, setDisplayName, roomServiceSocket, emitEvent }) => {
+import { Typography } from '@material-ui/core'
 
-    const { username } = useParams();
-    const [formDisplayName, setFormDisplayName] = useState('');
+const DisplayNameSubpage = ({
+  displayName,
+  setDisplayName,
+  roomServiceSocket,
+  emitEvent,
+}) => {
+  const { username } = useParams()
+  const [formDisplayName, setFormDisplayName] = useState('')
 
-    useEffect(() => {
-        const value = localStorage.getItem('displayName')
-        if (value) {
-            setFormDisplayName(value)
-        }
-    }, [])
-
-    const submit = async () => {
-        localStorage.setItem('displayName', formDisplayName);
-        setDisplayName(formDisplayName)
-        emitEvent(
-            'join_room',
-            {
-                roomID: username,
-                nickname: formDisplayName
-            }
-        );
+  useEffect(() => {
+    const value = localStorage.getItem('displayName')
+    if (value) {
+      setFormDisplayName(value)
     }
+  }, [])
 
-    const displayNameInvalid = (formDisplayName.length === 0)
-    return (
-        <ResponsiveContainer>
-            <form
-                onSubmit={(event) => {
-                        event.preventDefault(); 
-                        submit()
-                    }
-                }>
-                <TextField
-                    label='Display Name'
-                    value={formDisplayName}
-                    required
-                    error={displayNameInvalid}
-                    onChange={event => setFormDisplayName(event.target.value)}
-                />
-                <Button type='submit' disabled={displayNameInvalid || !!displayName}>
-                    Continue
-                </Button>
-                {
-                    !!displayName ? (<CircularProgress/>) : null
-                }
-            </form>
-        </ResponsiveContainer>
-    )
+  const submit = async () => {
+    localStorage.setItem('displayName', formDisplayName)
+    setDisplayName(formDisplayName)
+    emitEvent('join_room', {
+      roomID: username,
+      nickname: formDisplayName,
+    })
+  }
+
+  const displayNameInvalid = formDisplayName.length === 0
+  return (
+    <div className='App'>
+      <div className='radiant-background'>
+        <div className='login'>
+          <div className='container'>
+            <div className='base-container'>
+              <div className='content'>
+                <div className='image' style={{ marginTop: 30 }}>
+                  <img src={mainImg} />
+                </div>
+                <Typography variant='h1' color='primary'>
+                  PilePlan
+                </Typography>
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault()
+                    submit()
+                  }}
+                >
+                  <div style={{ marginTop: 40 }}>
+                    <TextField
+                      variant='filled'
+                      margin='dense'
+                      validators={['required']}
+                      errorMessages={['Display Name is required']}
+                      InputProps={{ disableUnderline: true }}
+                      fullWidth
+                      label='Display Name'
+                      value={formDisplayName}
+                      required
+                      error={displayNameInvalid}
+                      onChange={(event) =>
+                        setFormDisplayName(event.target.value)
+                      }
+                    />
+                    <Button
+                      variant='contained'
+                      color='primary'
+                      fullWidth
+                      style={{
+                        fontSize: 15,
+                      }}
+                      type='submit'
+                      disabled={displayNameInvalid || !!displayName}
+                    >
+                      Continue
+                    </Button>
+                  </div>
+                  {!!displayName ? <CircularProgress /> : null}
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
@@ -68,7 +106,7 @@ const mapStateToProps = (state) => {
     roomServiceSocket: roomServiceSocketSelector(state),
     displayName: displayNameSelector(state),
   }
-};
+}
 
 const mapDispatchToProps = {
   createRoomServiceConnection,
