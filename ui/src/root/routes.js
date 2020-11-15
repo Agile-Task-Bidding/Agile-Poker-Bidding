21153a1d088e28732d9758da48b6fdbac799bbe7
Ryan <ryan.cfplus@gmail.com>
2020-11-14 22:27:36 -0500

    re-add routes

--- ui/src/root/routes.js
+++ ui/src/root/routes.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import DemoPage from '../pages/DemoPage';
+import CreatePage from '../pages/create/CreatePage';
+import RoundPage from '../pages/round/RoundPage';
 
 export default (
   <Switch>
     <Route exact path='/home' component={HomePage} />
-    <Route exact path='/demo' component={DemoPage} />
+    <Route exact path='/login' component={HomePage} />
+    <Route exact path='/create' component={CreatePage} />
+    <Route exact path='/room/:username' component={RoundPage} />
   </Switch>
 )
