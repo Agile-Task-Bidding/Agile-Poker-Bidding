5260b80ee45a10bd45585ea5a3f5f6e1d2025b53
magdalenapasternak <maggpass@gmail.com>
2020-11-22 10:56:45 -0500

    changes to the design

--- ui/src/pages/round/ClosedSubpage.js
+++ ui/src/pages/round/ClosedSubpage.js
@@ -1,8 +1,50 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React from 'react'
+import { Typography } from '@material-ui/core'
+import { TextField, Button, CircularProgress } from '@material-ui/core'
+import mainImg from '../../components/icon/logo.svg'
+import { Link } from 'react-router-dom'
 
 const ClosedSubpage = (props) => {
-    return <Typography>The room has been closed</Typography>
-};
+  return (
+    <div className='App '>
+      <div className='radiant-background'>
+        <div className='login'>
+          <div className='container'>
+            <div className='base-container'>
+              <div className='content'>
+                <div className='image' style={{ marginTop: 30 }}>
+                  <img src={mainImg} />
+                </div>
+                <Typography variant='h1' color='primary'>
+                  PilePlan
+                </Typography>
 
-export default ClosedSubpage;
+                <div style={{ marginTop: 30 }}>
+                  <Typography variant='h6' color='primary'>
+                    This room has been closed
+                  </Typography>
+
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    fullWidth
+                    style={{
+                      fontSize: 15,
+                    }}
+                    type='submit'
+                    component={Link}
+                    to={'/'}
+                  >
+                    Home Page
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ClosedSubpage
--- ui/src/pages/round/MemberRow.js
+++ ui/src/pages/round/MemberRow.js
@@ -1,59 +1,104 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { Check, Close } from '@material-ui/icons';
-import { StyleSheet, css } from 'aphrodite';
-import { Button, Typography, Paper } from '@material-ui/core';
-import { accountSelector } from '../../data/state/account/account.selector';
+import React from 'react'
+import { connect } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { Check, Close } from '@material-ui/icons'
+import { StyleSheet, css } from 'aphrodite'
+import { Button, Typography, Paper } from '@material-ui/core'
+import { accountSelector } from '../../data/state/account/account.selector'
 import { roomServiceSocketSelector } from '../../data/state/room-service/room-service.selectors'
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
+import DeleteIcon from '@material-ui/icons/Delete'
 
-const MemberRow = ({ account, displayName, socketID, voted, roomServiceSocket, ...thruProps }) => {
+const MemberRow = ({
+  account,
+  displayName,
+  socketID,
+  voted,
+  roomServiceSocket,
+  ...thruProps
+}) => {
+  const { username } = useParams()
+  const isAdmin = account && account.username === username
 
-    const { username } = useParams();
-    const isAdmin = (account && account.username === username);
-
-    return (
-        <div className={css(styles.container)} {...thruProps}>
-            <Typography>{displayName}</Typography>
-            { voted ? <Check/> : <Close/>}
-            { isAdmin ? (
-                <Button
-                    onClick={async () => {
-                        console.log({
-                            idToken: await firebase.auth().currentUser.getIdToken(false),
-                            roomID: username,
-                            user: { socketID }
-                        })
-                        roomServiceSocket.emit('kick_user', {
-                            idToken: await firebase.auth().currentUser.getIdToken(false),
-                            roomID: username,
-                            user: { socketID }
-                        });
-                    }}
-                >
-                    Kick
-                </Button>) : 
-            null}
-        </div>
-    )
-};
-
-const styles = StyleSheet.create({
-    container: {
-        display: 'grid',
-        gridTemplateColumns: '2fr 1fr 1fr',
-        justifyContent: 'flex-start',
-        width: '100%',
-    }
-});
-
-const mapStateToProps = (state) => {
-    return {
-        account: accountSelector(state),
-        roomServiceSocket: roomServiceSocketSelector(state),
-    }
+  return (
+    <div className={css(styles.container)} {...thruProps}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'row',
+          justifyContent: 'center',
+        }}
+      >
+        {voted ? (
+          <CheckCircleIcon
+            style={{
+              color: '#223496',
+            }}
+            fontSize='large'
+          />
+        ) : (
+          <CheckCircleOutlineIcon
+            style={{
+              color: '#223496',
+            }}
+            fontSize='large'
+          />
+        )}
+        <Typography
+          style={{
+            color: '#223496',
+            paddingTop: '5px',
+          }}
+          variant='h5'
+        >
+          {displayName}
+        </Typography>
+      </div>
+      {isAdmin ? (
+        <Button
+          onClick={async () => {
+            console.log({
+              idToken: await firebase.auth().currentUser.getIdToken(false),
+              roomID: username,
+              user: { socketID },
+            })
+            roomServiceSocket.emit('kick_user', {
+              idToken: await firebase.auth().currentUser.getIdToken(false),
+              roomID: username,
+              user: { socketID },
+            })
+          }}
+        >
+          <DeleteIcon
+            style={{
+              color: '#223496',
+            }}
+            fontSize='large'
+          />
+        </Button>
+      ) : null}
+    </div>
+  )
 }
 
-export default connect(mapStateToProps, {})(MemberRow);
+const styles = StyleSheet.create({
+  container: {
+    display: 'grid',
+    // gridTemplateColumns: '2fr 1fr',
+    justifyContent: 'flex-start',
+    width: '100%',
+  },
+})
+
+const mapStateToProps = (state) => {
+  return {
+    account: accountSelector(state),
+    roomServiceSocket: roomServiceSocketSelector(state),
+  }
+}
+
+export default connect(mapStateToProps, {})(MemberRow)
