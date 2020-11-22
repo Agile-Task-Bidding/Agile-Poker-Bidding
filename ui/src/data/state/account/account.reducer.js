2fe8dc5e39a44fd726a78976fc31a468115508d2
Ryan <ryan.cfplus@gmail.com>
2020-11-21 20:59:57 -0500

    merge fixes

--- ui/src/data/state/account/account.reducer.js
+++ ui/src/data/state/account/account.reducer.js
@@ -2,11 +2,7 @@ import * as types from '../action-types'
 import GameState from '../../../services/GameState'
 
 const initialState = {
-<<<<<<< HEAD
-    account: null
-=======
   account: null,
->>>>>>> origin/APB-56-Styling-cards-result
 }
 
 export default function (state = initialState, action) {
