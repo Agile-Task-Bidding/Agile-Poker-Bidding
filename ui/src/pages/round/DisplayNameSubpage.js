0cff0b52e02bd39b7a362f0f16fc1e1b7fdb3754
Ryan <ryan.cfplus@gmail.com>
2020-12-04 15:50:05 -0500

    bug fix with state

--- ui/src/pages/round/DisplayNameSubpage.js
+++ ui/src/pages/round/DisplayNameSubpage.js
@@ -28,6 +28,8 @@ const DisplayNameSubpage = ({
     const value = localStorage.getItem('displayName')
     if (value) {
       setFormDisplayName(value)
+    } else {
+      setFormDisplayName('');
     }
   }, [])
 
